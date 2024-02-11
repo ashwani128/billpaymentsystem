9094900f000bb3409a2929ad9b0c9943a7bc63b0
ashwani <ashwanisrivstva@gmail.com>
2024-02-11 14:04:20 +0530

    Add pipes and table form dynamcall add Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -21,12 +22,24 @@ export class AppComponent {
 
   marks: number = 10;
   color: string = "";
-  myClass:string = "";
-  b:boolean = false;
+  myClass: string = "";
+  b: boolean = false;
 
-  country:string = "";
-  cities:string[] = ["Ayodhya","kanpur","Lucknow","Noida"];
+  country: string = "";
+  cities: string[] = ["Ayodhya", "kanpur", "Lucknow", "Noida"];
+  employee: Employee[] = [new Employee("1", "test1", "50000"),
+  new Employee("2", "Rocklee", "6000"),
+  new Employee("3", "Rocklee", "6000")];
 
+  newEmployee: Employee = new Employee("", "", "");
+
+  // forusing pipes
+
+  city:string = "Kanpur";
+  newSalary:number = 2000;
+  tds:number = 54.5;
+  person:object = {firstname:"Rocklee",lastname:"gateofdeath"};
+  dt:Date = new Date();
 
   constructor() {
 
@@ -50,20 +63,7 @@ export class AppComponent {
       this.myClass = "class2";
     }
 
-
-
-
-
-
-
-
-
   }
-
-
-
-
-
 
   handleSubmitButton() {
     this.fName = "Mighty Guy";
@@ -73,7 +73,17 @@ export class AppComponent {
     this.gender = "Male";
   }
 
+  onClickDelete(n: number) {
+    alert("1")
+    if (confirm("Are you sure you want to delete")) {
+      this.employee.splice(n, 1);
+    }
+  }
 
-
-
+  onClickInsert() {
+    this.employee.push(new Employee(this.newEmployee.empId, this.newEmployee.empName, this.newEmployee.salary));
+    this.newEmployee.empId = "0";
+    this.newEmployee.empName = "";
+    this.newEmployee.salary = "0";
+  }
 }
